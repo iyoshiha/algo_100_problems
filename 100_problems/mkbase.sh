dfcabb17129ade4d18d29df56c71fd83e1ccf484
iyoshiha <yoshi3co@gmail.com>
2022-02-20 19:21:29 +0900

    cd added

--- 100_problems/mkbase.sh
+++ 100_problems/mkbase.sh
@@ -4,4 +4,5 @@ echo "put the directory name :"
 read DirectoryName
 cp -r base "./$DirectoryName"
 git checkout -b $DirectoryName
+cd "`pwd`/$DirectoryName"
 echo "`ls | grep $DirectoryName` directory and branch is created!"

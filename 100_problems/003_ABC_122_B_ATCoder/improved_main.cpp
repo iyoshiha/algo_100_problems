1994492fdff1b5e49c82f30cf450f5e92b01a09f
iyoshiha <yoshi3co@gmail.com>
2022-02-21 00:20:36 +0900

    i

--- 100_problems/003_ABC_122_B_ATCoder/improved_main.cpp
+++ 100_problems/003_ABC_122_B_ATCoder/improved_main.cpp
@@ -6,17 +6,15 @@ using namespace std;
 int main(void)
 {
     int max;
-    int i;
     string str;
     int count;
 
-    i = 0;
     max = 0;
     count = 0;
     cin >> str;
     for (char c : str)
     {
-        if (str[i] == 'A' || str[i] == 'C' || str[i] == 'G' || str[i] == 'T')
+        if (c == 'A' || c == 'C' || c == 'G' || c == 'T')
         {
             count++;
             if (max < count)

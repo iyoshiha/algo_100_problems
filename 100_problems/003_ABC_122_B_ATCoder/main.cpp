f167b71e539f541197b63e1a53a6aa2b06b79fa4
iyoshiha <yoshi3co@gmail.com>
2022-02-20 23:33:49 +0900

    debuged

--- 100_problems/003_ABC_122_B_ATCoder/main.cpp
+++ 100_problems/003_ABC_122_B_ATCoder/main.cpp
@@ -1,8 +1,49 @@
 #include <iostream>
+#include <string>
+
 using namespace std;
 
 int main(void)
 {
+    int max;
+    int i, j;
+    string substr = "ACGT";
+    string str;
+    int count;
+    bool match;
+
+    cin >> str;
+
+    i = 0;
+    j = 0;
+    max = 0;
+    count = 0;
+    while (str[i])
+    {
+        match = false;
+        j = 0;
+        while (substr[j])
+        {
+            if (str[i] == substr[j])
+            {
+            cout << "str[" <<i << "] = " << str[i] <<" sub[" <<j << "] = "  << substr[j] << endl;
+                count++;
+                match = true;
+                // if (max < count)
+                //     max = count;
+                break;
+            }
+            j++;
+        }
+        if (match = true)
+            if (max < count)
+                max = count;
+        else
+            count = 0;
+        cout << "count is :" << count << endl;
+        i++;
+    }
+    cout << max << endl;
 
     return 0;
 }

40e07ded00e6dc24b1ce15e21abc28963b88fb75
iyoshiha <yoshi3co@gmail.com>
2022-02-21 03:50:43 +0900

    for lop

--- 100_problems/010_ADLDS_5_A_BruteForce_bitSearch/main.cpp
+++ 100_problems/010_ADLDS_5_A_BruteForce_bitSearch/main.cpp
@@ -1,11 +1,19 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#define For(i, times) for(int (i) = 0; times > (i); (i++))
+
 
 using namespace std;
 
 int main(void)
 {
+    int n; cin >> n;
+    int A[n]; For(i, n) cin >> A[i];
+    int q; cin >> q;
+    int M[q]; For(i, q) cin >> M[i];
+    int i;
+
 
     return 0;
 }

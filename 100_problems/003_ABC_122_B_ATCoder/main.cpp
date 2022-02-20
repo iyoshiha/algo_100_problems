a4a81fc65675d10dd272c6c6e8a424bead614979
iyoshiha <yoshi3co@gmail.com>
2022-02-20 21:24:22 +0900

    main init

---
+++ 100_problems/003_ABC_122_B_ATCoder/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+
+    return 0;
+}
+
+/**************************
+link of the problem:
+https://atcoder.jp/contests/abc122/tasks/abc122_b
+***************************/

958f9da202656ee59b2477e1159baa922bd3f565
iyoshiha <yoshi3co@gmail.com>
2022-02-20 18:09:29 +0900

    Accepted

---
+++ 100_problems/001_ITP1_7_8_HowManyWays/main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n[1024] = {0};
+    int x[1024] = {0};
+    int max = 0;
+    int i, j, k, l;
+    int ans = 0;
+
+    do {
+    cin>> n[max];
+    cin>> x[max];
+    max++;
+    } while (n[max - 1] != 0 || x[max - 1] != 0);
+    max--;
+
+    l = 0;
+    while (max > l)
+    {
+        ans = 0;
+        i = 1;
+        while (i < n[l] - 1)
+        {
+            j = i + 1;
+            while (j < n[l])
+            {
+                k = j + 1;
+                while (k <= n[l])
+                {
+                    if (x[l] == i + j + k)
+                        ans++;
+                    k++;
+                }
+                j++;
+            }
+            i++;
+        }
+        cout << ans << endl;
+        l++;
+    }
+    return 0;
+}

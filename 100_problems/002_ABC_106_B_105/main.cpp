8a9413563450e7ca982cbf84048aff052672117c
iyoshiha <yoshi3co@gmail.com>
2022-02-20 20:03:43 +0900

    char is not properly displayed

--- 100_problems/002_ABC_106_B_105/main.cpp
+++ 100_problems/002_ABC_106_B_105/main.cpp
@@ -3,7 +3,30 @@ using namespace std;
 
 int main(void)
 {
+    unsigned char n;
+    unsigned char i, j;
+    unsigned char divisor_count;
+    unsigned char ans;
 
+    cin >> n;
+    cout << n << endl;
+    ans = 0;
+    i = 2;
+    while (n >= i)
+    {
+        divisor_count = 2;
+        j = 2;        // i self and 1 is already counted;
+        while (i > j) // i self and 1 is already counted;
+        {
+            if (i % j == 0)
+                divisor_count++;
+            j++;
+        }
+        if (divisor_count == 8)
+            ans++;
+        i += 2;
+    }
+    // cout << ans << endl;
     return 0;
 }
 

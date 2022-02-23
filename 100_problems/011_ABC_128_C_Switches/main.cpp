232677364f68fa1f95775afb3a3ead16f126ef5b
iyoshiha <yoshi3co@gmail.com>
2022-02-24 04:20:48 +0900

    for shcool

--- 100_problems/011_ABC_128_C_Switches/main.cpp
+++ 100_problems/011_ABC_128_C_Switches/main.cpp
@@ -6,13 +6,13 @@ using namespace std;
 
 int main(void)
 {
-
+    
     return 0;
 }
 
 /**************************
 link of the problem:
-
+https://atcoder.jp/contests/apg4b/tasks/APG4b_r?lang=ja
 
 Filesize: Byte
 Runtime :   ms

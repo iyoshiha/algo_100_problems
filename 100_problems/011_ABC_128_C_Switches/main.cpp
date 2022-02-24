c364b3d6953546b9980de73b26a9ebf274a669b1
iyoshiha <yoshi3co@gmail.com>
2022-02-25 05:37:57 +0900

    get stdinput

--- 100_problems/011_ABC_128_C_Switches/main.cpp
+++ 100_problems/011_ABC_128_C_Switches/main.cpp
@@ -6,9 +6,28 @@ using namespace std;
 
 int main(void)
 {
-    
+    int        N, M, K;
+    cin >> N; cin >> M;
+    int        s[M][N + 1];
+    int        p[M];
+
+    for (int i = 0; i < M; i++)
+    {
+        for (int j = 1; i < K; j++)
+        {
+            cin >> s[i][0];
+            cin >> s[i][j];
+        }
+    }
+    for (int i = 0; i < M; i++)
+    {
+    cin >> p[i];
+    }
+
+
     return 0;
 }
+
 
 /**************************
 link of the problem:

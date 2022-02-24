8af2916bfd10b4037040a4b9ced9ede1931e61db
iyoshiha <yoshi3co@gmail.com>
2022-02-25 07:14:40 +0900

    j

--- 100_problems/011_ABC_128_C_Switches/main.cpp
+++ 100_problems/011_ABC_128_C_Switches/main.cpp
@@ -3,17 +3,24 @@
 #include <algorithm>
 
 using namespace std;
-
+#define ON 1
+#define OFF 0
+#define NON 2
+#define light_is_on 1
 int main(void)
 {
-    int        N, M, K;
-    cin >> N; cin >> M;
-    int        s[M][N + 1];
-    int        p[M];
+    int N, M, K;
+    cin >> N;
+    cin >> M;
+    int s[M][N + 1];
+    int p[M];
+    int table[M][M][N] = {NON};
+    int count;
+    int cal;
 
-    for (int i = 0; i < M; i++)
+        for (int i = 0; i < M; i++)
     {
-        for (int j = 1; i < K; j++)
+        for (int j = 1; j < K; j++)
         {
             cin >> s[i][0];
             cin >> s[i][j];
@@ -24,6 +31,25 @@ int main(void)
     cin >> p[i];
     }
 
+    int l = 0;
+    for (int m = 0; m < M; m++)
+    {
+    for (int bit = 0; bit <= (1 << K); bit++)
+    {
+        cal = 0;
+        for (int i = 0; i < K; i++)
+        {
+        cal += bit & (1 << i);
+        if (light_is_on)
+        for (int k = 0; k < K; k++)
+        {
+            table[m][l][k] = bit & (1 << k);
+        }
+        l++;        
+        }
+    }
+
+    }
 
     return 0;
 }
@@ -31,7 +57,7 @@ int main(void)
 
 /**************************
 link of the problem:
-https://atcoder.jp/contests/apg4b/tasks/APG4b_r?lang=ja
+https://atcoder.jp/contests/abc128/tasks/abc128_c
 
 Filesize: Byte
 Runtime :   ms
